ad3539587c82ec2df936b3f6cc7a2a4525c3bdea
maivankien <maivankien1710@gmail.com>
2022-12-26 17:22:14 +0700

    Update code

--- src/app/models/Course.js
+++ src/app/models/Course.js
@@ -1,13 +1,15 @@
 const mongoose = require('mongoose');
 const slug = require('mongoose-slug-generator');
 const mongooseDelete = require('mongoose-delete');
+const AutoIncrement = require('mongoose-sequence')(mongoose);
 
 mongoose.set('strictQuery', false);
 
 const Schema = mongoose.Schema;
 
-const Course = new Schema(
+const CourseSchema = new Schema(
     {
+        _id: { type: Number },
         name: { type: String, require: true },
         description: { type: String, maxLength: 600 },
         image: { type: String, maxLength: 255 },
@@ -17,15 +19,17 @@ const Course = new Schema(
         slug: { type: String, slug: 'name', unique: true },
     },
     {
+        _id: false,
         timestamps: true,
     },
 );
 
 // Add plugins
 mongoose.plugin(slug);
-Course.plugin(mongooseDelete, {
+CourseSchema.plugin(AutoIncrement);
+CourseSchema.plugin(mongooseDelete, {
     deletedAt: true,
     overrideMethods: 'all',
 });
 
-module.exports = mongoose.model('Course', Course);
+module.exports = mongoose.model('Course', CourseSchema);
--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ const morgan = require('morgan');
 const { engine } = require('express-handlebars');
 const methodOverride = require('method-override');
 
+const { SortMiddlewares } = require('./app/middlewares/SortMiddlewares');
+
 const app = express();
 const port = 3000;
 
@@ -24,6 +26,9 @@ app.use(express.json());
 
 app.use(methodOverride('_method'));
 
+// Custom middlewares
+app.use(SortMiddlewares);
+
 // HTTP logger
 // app.use(morgan('combined'))
 
@@ -34,6 +39,25 @@ app.engine(
         extname: '.hbs',
         helpers: {
             sum: (a, b) => a + b,
+            sortable: (field, sort) => {
+                const sortType = field === sort.column ? sort.type : 'default';
+
+                const icons = {
+                    default: 'oi oi-elevator',
+                    asc: 'oi oi-sort-ascending',
+                    desc: 'oi oi-sort-descending',
+                };
+                const types = {
+                    default: 'desc',
+                    asc: 'desc',
+                    desc: 'asc',
+                };
+
+                const type = types[sortType];
+                const icon = icons[sortType];
+
+                return `<a href="?_sort&column=${field}&type=${type}"><span class="${icon}"></span></a>`;
+            },
         },
     }),
 );
